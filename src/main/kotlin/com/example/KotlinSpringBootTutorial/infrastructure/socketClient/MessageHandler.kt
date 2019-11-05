e2d51cf10b06406326c0a64381e0da44154747a8
sho0126hiro <sho0126hiro@gmail.com>
2019-11-05 14:13:06 +0900

    implement to send Simple Text Message with WebSocket.

---
+++ src/main/kotlin/com/example/KotlinSpringBootTutorial/infrastructure/socketClient/MessageHandler.kt
@@ -0,0 +1,4 @@
+package com.example.KotlinSpringBootTutorial.infrastructure.socketClient
+
+class MessageHandler {
+}
--- src/main/kotlin/com/example/KotlinSpringBootTutorial/infrastructure/socketClient/SocketConfig.kt
+++
@@ -1,9 +0,0 @@
-package com.example.KotlinSpringBootTutorial.infrastructure.socketClient
-
-object SocketConfig {
-    object Connection {
-        object Test {
-            const val PORT_NUMBER: Int = 3000
-        }
-    }
-}
